e7a50733e9985b2becfc4b1a03eddf9c546ba05c
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 14:15:35 -0400

    Criação classe ControllerExceptionHandler

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.picpaysimplificado.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.picpaysimplificado.picpaysimplificado.dtos.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDpuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(EntityNotFoundException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
