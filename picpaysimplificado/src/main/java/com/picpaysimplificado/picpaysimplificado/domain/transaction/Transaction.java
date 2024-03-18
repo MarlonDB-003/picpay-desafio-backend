d35a533732090ac328a4919ea6ba41e16bf8b4b6
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 10:01:16 -0400

    criação das classes de usuário e transação

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,40 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToAny
+    @JoinColumn(name="sander_id")
+    private User sender;
+    @ManyToAny
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+    
+}
