eb8892c16d2f69941d34689771dc926110e996d6
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 11:15:30 -0400

    update na classe Transaction

--- picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -16,6 +16,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity(name="transactions")
@@ -23,6 +24,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class Transaction {
     @Id
