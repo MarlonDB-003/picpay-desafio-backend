ed848c8f24ba821f8b01e8626a7b3566fea4bb26
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 11:48:54 -0400

    Update classe UserService

--- picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
@@ -1,12 +1,14 @@
 package com.picpaysimplificado.picpaysimplificado.services;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.picpaysimplificado.repositories.UserRepository;
 
 @Service
@@ -26,6 +28,16 @@ public class UserService {
 
     public User findUserById(Long id) throws Exception{
         return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public User createUser (UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser; 
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
     }
 
     public void saveUser(User user){
