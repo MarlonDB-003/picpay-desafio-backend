30fd1da31e01e0d835c3c7035b44a88974c1cfab
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 11:15:02 -0400

    criação da classe NotificationService

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,29 @@
+package com.picpaysimplificado.picpaysimplificado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.picpaysimplificado.dtos.NotificationDTO;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception{
+        String email = user.getEmail();
+
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse= restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
+    
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Erro ao enviar notificação");
+            throw new Exception("Serviço de notificação está fora do ar");
+        }
+    }
+}
