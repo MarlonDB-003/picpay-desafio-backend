b59abf102d90b908f7076ab15d78b56b8308bb6e
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 14:16:48 -0400

    Update ControllerExceptionHandler

--- picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/infra/ControllerExceptionHandler.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -23,7 +23,7 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity threatGeneralException(EntityNotFoundException exception){
+    public ResponseEntity threatGeneralException(Exception exception){
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
         return ResponseEntity.internalServerError().body(exceptionDTO);
     }
