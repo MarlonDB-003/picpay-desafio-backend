64b598a7edc3cf638aa49d8eb1cb1b6a9e8d8376
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-18 10:14:11 -0400

    criação da classe UserRepository

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado.picpaysimplificado.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
